fe6426838cd0bacb4278e36a54f9007ca937a788
Aleksey Shulikov <60194526+D1scoDancer@users.noreply.github.com>
2020-03-31 12:45:00 +0300

    Plan to make MovingPlatform

--- FrogController.cs
+++ FrogController.cs
@@ -1,8 +1,5 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
 using UnityEngine;
-using System.Timers;
-using System;
 
 public class FrogController : MonoBehaviour
 {
---
+++ MovingPlatform.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+
+    [SerializeField]
+    float speed;
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        transform.Translate(Time.deltaTime * speed, Time.deltaTime * speed, 0);
+    }
+}
