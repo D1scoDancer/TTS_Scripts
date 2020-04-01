7088b4da670635b5e5103012eacf123a256fc921
Aleksey Shulikov <60194526+D1scoDancer@users.noreply.github.com>
2020-04-01 14:53:29 +0300

    Moving Platform now works

--- MovingPlatform.cs
+++ MovingPlatform.cs
@@ -7,17 +7,43 @@ public class MovingPlatform : MonoBehaviour
 
     [SerializeField]
     private float speed;
-    
+
+    [SerializeField]
+    private bool movingRight;
+
+    [SerializeField]
+    private int xDirection;
+
+    [SerializeField]
+    private int yDirection;
+
+
+
     private void Start()
     {
-        
+
     }
 
-    
     private void Update()
     {
-        transform.Translate(Time.deltaTime * speed, -Time.deltaTime * speed, 0);
+        if(movingRight)
+        {
+            transform.Translate(Time.deltaTime * speed * xDirection,
+                Time.deltaTime * speed * yDirection, 0);
+        }
+        else
+        {
+            transform.Translate(-Time.deltaTime * speed * xDirection,
+                -Time.deltaTime * speed * yDirection, 0);
+        }
+
     }
 
-    
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.CompareTag("Turn"))
+        {
+            movingRight = !movingRight;
+        }
+    }
 }
