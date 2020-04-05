8e11cb7087c9eb131643698729ac39db1d89449d
Aleksey Shulikov <60194526+D1scoDancer@users.noreply.github.com>
2020-04-05 21:41:00 +0300

    WaterWheelPlatform Created
    Платформа которая двигается по кругу (квадрату)

--- Platforms/WaterWheelPlatform.cs
+++ Platforms/WaterWheelPlatform.cs
@@ -4,15 +4,104 @@ using UnityEngine;
 
 public class WaterWheelPlatform : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private float speed;
+
+    [SerializeField]
+    private bool activatedByPlayer;
+
+    public bool movingRight;
+
+    [Range(0, 1)]
+    private int xMultiplier;
+
+    [Range(0, 1)]
+    private int yMultiplier;
+
+    private void Start()
     {
-        
+        if(movingRight)
+        {
+            xMultiplier = 0;
+            yMultiplier = 1;
+        }
+        else
+        {
+            xMultiplier = 0;
+            yMultiplier = -1;
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if(activatedByPlayer)
+        {
+            if(transform.Find("Player") != null)
+            {
+                StartWheel();
+                activatedByPlayer = false;
+            }
+        }
+        else
+        {
+            StartWheel();
+        }
+    }
+
+    private void StartWheel()
+    {
+        if(movingRight)
+        {
+            transform.Translate(Time.deltaTime * speed * xMultiplier,
+                Time.deltaTime * speed * yMultiplier, 0);
+        }
+        else
+        {
+            transform.Translate(Time.deltaTime * speed * xMultiplier,
+                Time.deltaTime * speed * yMultiplier, 0);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("WheelCommand"))
+        {
+            if(movingRight)
+            {
+                switch(collision.gameObject.name)
+                {
+                    case "WheelCommand1":
+                        xMultiplier = 1; yMultiplier = 0;
+                        break;
+                    case "WheelCommand2":
+                        xMultiplier = 0; yMultiplier = -1;
+                        break;
+                    case "WheelCommand3":
+                        xMultiplier = -1; yMultiplier = 0;
+                        break;
+                    case "WheelCommand4":
+                        xMultiplier = 0; yMultiplier = 1;
+                        break;
+                }
+            }
+            else
+            {
+                switch(collision.gameObject.name)
+                {
+                    case "WheelCommand1":
+                        xMultiplier = 0; yMultiplier = -1;
+                        break;
+                    case "WheelCommand2":
+                        xMultiplier = -1; yMultiplier = 0;
+                        break;
+                    case "WheelCommand3":
+                        xMultiplier = 0; yMultiplier = 1;
+                        break;
+                    case "WheelCommand4":
+                        xMultiplier = 1; yMultiplier = 0;
+                        break;
+                }
+            }
+        }
     }
 }
--- Player/Player.cs
+++ Player/Player.cs
@@ -179,7 +179,7 @@ public class Player : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.name.Contains("MovingPlatform"))
+        if(collision.gameObject.name.Contains("Platform"))
         {
             this.transform.parent = collision.transform;
         }
