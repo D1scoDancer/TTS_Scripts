c8690aabef7e7fbc9e1c9815802a4f3897990e8f
Aleksey Shulikov <60194526+D1scoDancer@users.noreply.github.com>
2020-04-05 19:57:26 +0300

    Death Tunnel created

---
+++ DeathTunnel.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathTunnel : MonoBehaviour
+{
+
+    public float speed;
+
+    public bool trap;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.name == "Player")
+        {
+            trap = true;
+        }
+    }
+
+    private void Update()
+    {
+        if(trap)
+        {
+            transform.Find("LongPlatformUp").transform.Translate(0, Time.deltaTime * speed, 0);
+        }
+    }
+
+}
---
+++ DeathTunnelChild.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathTunnelChild : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log(1);
+        if(collision.gameObject.name == "LongPlatformDown")
+            transform.parent.GetComponent<DeathTunnel>().trap = false;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log(2);
+    }
+
+   
+}
+
