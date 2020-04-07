afbaf5840bcc7885d14fbff47a8744b1fbc3f35d
Aleksey Shulikov <60194526+D1scoDancer@users.noreply.github.com>
2020-04-07 12:10:57 +0300

    Create AutoDestroy.cs

---
+++ AutoDestroy.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoDestroy : MonoBehaviour
+{
+    public double life = 1f;
+    void  Start()
+    {
+        life = Time.time + 1.0;
+    }
+
+    void Update()
+    {
+        if(life <= Time.time)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
