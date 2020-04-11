27de781666d22b51ea5c8814162cf05b7d2e22c3
Aleksey Shulikov <60194526+D1scoDancer@users.noreply.github.com>
2020-04-11 20:16:13 +0300

    Update Player.cs

--- Player/Player.cs
+++ Player/Player.cs
@@ -6,7 +6,6 @@ public class Player : MonoBehaviour
 {
     private Rigidbody2D myRigidbody2D;
     private Animator myAnimator;
-    private BoxCollider2D myBoxCollider2D;
 
     [SerializeField]
     private LayerMask whatIsGround;
