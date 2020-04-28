09f8713d2f772d845eaf041ee8a2840417722b76
Aleksey Shulikov <60194526+D1scoDancer@users.noreply.github.com>
2020-04-28 13:24:38 +0300

    Now spider is waiting for player to come closer to start a fight

---
+++ Enemies/Spider/SpiderWaiter.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class SpiderWaiter : MonoBehaviour
+{
+    public SpiderFighter spiderFighter;
+    public GameObject player;
+    void Start()
+    {
+        transform.position = new Vector3(3788f, 231.5f, 10f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Mathf.Abs(player.transform.position.x - transform.position.x) < 200)
+        {
+            spiderFighter.enabled = true;
+        }
+        else
+        {
+            spiderFighter.enabled = false;
+            transform.position = new Vector3(3788f, 231.5f, 10f);
+        }
+    }
+}
