b87d2e3759b7baf3cda41dc7d0544c781e9ee854
Aleksey Shulikov <60194526+D1scoDancer@users.noreply.github.com>
2020-05-03 18:12:44 +0300

    Update ImpulseBox.cs

--- ImpulseBox.cs
+++ ImpulseBox.cs
@@ -1,25 +1,11 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class ImpulseBox : MonoBehaviour
 {
-    public float forceX;
     public float forceY;
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        collision.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(forceX, forceY));
+        collision.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, forceY));
     }
 }
