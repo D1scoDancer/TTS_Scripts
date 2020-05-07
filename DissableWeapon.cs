0095bcee4d30617fdc14d1f6c600e1dc23ceaf5d
Aleksey Shulikov <60194526+D1scoDancer@users.noreply.github.com>
2020-05-07 21:20:01 +0300

    nothing special

---
+++ DissableWeapon.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class DissableWeapon : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.name == "Player")
+        {
+            collision.GetComponent<Weapon>().enabled = false;
+        }
+    }
+}
