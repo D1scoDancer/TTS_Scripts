2dc8de54b6c23b98bdff15469da268fcd6f7bf70
Aleksey Shulikov <60194526+D1scoDancer@users.noreply.github.com>
2020-05-09 14:18:18 +0300

    Update AudioManager.cs

--- Audio/AudioManager.cs
+++ Audio/AudioManager.cs
@@ -32,8 +32,6 @@ public class AudioManager : MonoBehaviour
             s.source.pitch = s.pitch;
             s.source.loop = s.loop;
         }
-
-      
     }
 
     public void Play(string name)
