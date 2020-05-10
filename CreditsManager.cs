09bb9fc5d6ecd0b655009f59b7cde3bb2bf737f9
Aleksey Shulikov <60194526+D1scoDancer@users.noreply.github.com>
2020-05-10 18:03:14 +0300

    Credits

---
+++ CreditsManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class CreditsManager : MonoBehaviour
+{
+    public float time;
+    public GameObject button;
+
+    private void Start()
+    {
+        StartCoroutine(Wait());
+    }
+
+    void Update()
+    {
+
+    }
+
+    IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(time);
+        button.SetActive(true);
+    }
+
+    public void GoToMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
